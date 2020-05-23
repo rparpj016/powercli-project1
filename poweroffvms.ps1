051fd7b9f19997e3a4800d40b624711696245798
rparpj016 <rparpj016@outlook.com>
2020-05-23 12:16:42 +0100

    my Second commit

--- poweroffvms.ps1
+++ poweroffvms.ps1
@@ -1,4 +1,5 @@
-###Poweroff VMS
+###Poweroff VMS###
+##Adding first comment
  ###############Power off  VMS###############
      # Add the VM names to a text file and save it on a location for example C:\Scripts as Vmnames.txt
      foreach($vmName in (Get-Content -Path C:\scripts\vmnames.txt)){
