6519da86f26c96f428d4550e9d33548fdd0b7fbc
rparpj016 <rparpj016@outlook.com>
2020-05-23 11:57:43 +0100

    My First every git File

---
+++ poweroffvms.ps1
@@ -0,0 +1,22 @@
+###Poweroff VMS
+ ###############Power off  VMS###############
+     # Add the VM names to a text file and save it on a location for example C:\Scripts as Vmnames.txt
+     foreach($vmName in (Get-Content -Path C:\scripts\vmnames.txt)){
+
+    $vm = Get-VM -Name $vmName
+
+    if($vm.Guest.State -eq "Running"){
+
+       
+       Shutdown-VMGuest -VM $vm -Confirm:$false
+
+    }
+
+    else{
+
+        Stop-VM -VM $vm -Confirm:$false
+
+    }
+
+}
+
