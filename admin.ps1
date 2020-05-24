c186871a007a9473cc7b03e43f2ab872113584cc
rparpj016 <rparpj016@outlook.com>
2020-05-24 22:10:12 +0100

    Change Notes

--- admin.ps1
+++ admin.ps1
@@ -4,7 +4,7 @@
 #Modified by : Roshan Joseph
 # This script will  will take computer names from text file and a random complex password is generated and it will check for whether server is online and will change the admin password .Passwords and server status are saved to a csv file.
 
-#Please note that user running script should have administrator privileges in the server.
+##Please note that user running script should have administrator privileges in the server.
 
 
 $computers = Get-Content -path "D:\Scripts\AdminPasswordchange\computers.txt"
